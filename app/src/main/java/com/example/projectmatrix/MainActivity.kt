5bfe28e29154960e4aee011ae622561655ea43ac
Ruslan <ruslan.zabauski@gmail.com>
2024-11-06 18:15:45 +0000

    adding a matrix to an application

--- app/src/main/java/com/example/projectmatrix/MainActivity.kt
+++ app/src/main/java/com/example/projectmatrix/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.projectmatrix
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.TextView
-import androidx.activity.ComponentActivity
 import android.widget.*
-
+import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
         val editTextName: EditText = findViewById(R.id.editTextName)
         val editTextSurname: EditText = findViewById(R.id.editTextSurname)
         val editTextPhone: EditText = findViewById(R.id.editTextPhone)
-
+        val imageView: ImageView = findViewById(R.id.imageView)
 
         myButton.setOnClickListener {
             myButton.visibility = View.GONE
@@ -32,7 +32,6 @@ class MainActivity : ComponentActivity() {
             submitButton.visibility = View.VISIBLE
         }
 
-
         submitButton.setOnClickListener {
             val name = editTextName.text.toString()
             val surname = editTextSurname.text.toString()
@@ -40,12 +39,30 @@ class MainActivity : ComponentActivity() {
 
             if (validateName(name) && validateSurname(surname) && validatePhone(phone)) {
                 hideKeyboard()
-                myTextView.text = "bla bla bla matrix matrix matrix"
-                myTextView.visibility = View.VISIBLE
+                myTextView.visibility = View.GONE
                 editTextName.visibility = View.GONE
                 editTextSurname.visibility = View.GONE
                 editTextPhone.visibility = View.GONE
                 submitButton.visibility = View.GONE
+
+                imageView.visibility = View.VISIBLE
+                imageView.setImageResource(R.drawable.test)
+                imageView.setOnTouchListener { _, event ->
+                    when (event.action) {
+                        MotionEvent.ACTION_DOWN -> {
+                            val drawable = imageView.drawable
+                            val imageWidth = drawable.intrinsicWidth
+                            val imageHeight = drawable.intrinsicHeight
+
+                            val normX = event.x / imageWidth
+                            val normY = event.y / imageHeight
+
+                            Toast.makeText(this, "$normX, $normY", Toast.LENGTH_SHORT).show()
+                            true
+                        }
+                        else -> false
+                    }
+                }
             } else {
                 Toast.makeText(this, "Please enter correct data.", Toast.LENGTH_SHORT).show()
             }
@@ -68,7 +85,7 @@ class MainActivity : ComponentActivity() {
 
     private fun validatePhone(phone: String): Boolean {
         return phone.matches(Regex("^\\+?\\d{10,}$"))
-    }   
+    }
 }
 
 
